1dac4c6ebe2eeaa46dc267698afa3034bd49f539
Vladimir Kuchumov <kuchumovds@gmail.com>
2022-03-03 09:59:59 +0300

    tele2

--- Bot_Duty.py
+++ Bot_Duty.py
@@ -4,8 +4,17 @@ import pandas as pd
 import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
-from config import Token, duty_message, unix_escalation, to_email, from_email, email_password, win_escalation, srk_escalation, shd_escalation, tele2_number, tele2_password
+from config import Token, duty_message, unix_escalation, to_email, from_email, email_password, win_escalation, srk_escalation, shd_escalation, tele2_number, tele2_password, Phones
 from array import *
+from telebot import types
+from selenium import webdriver
+#from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+#from webdriver_manager.chrome import ChromeDriverManager
+import os
+from selenium.webdriver.common.by import By
+import pickle
+from selenium.webdriver.common.keys import Keys
 
 task = []
 email_check = 0
@@ -27,6 +36,79 @@ def handle_start_command(message):
     local_time = time.localtime().tm_wday
     bot.send_message(message.from_user.id, f'{local_time}')
     #print(local_time)
+
+@bot.message_handler(commands=['phone'])
+def handle_start_command(message):
+    markup = types.InlineKeyboardMarkup(row_width=2)
+    item = types.InlineKeyboardButton('Лариса', callback_data=Phones[0])
+    item2 = types.InlineKeyboardButton('Руслан', callback_data=Phones[1])
+    item3 = types.InlineKeyboardButton('Глеб', callback_data=Phones[2])
+    item4 = types.InlineKeyboardButton('Саша', callback_data=Phones[3])
+    markup.add(item, item2, item3, item4)
+    bot.send_message(message.chat.id, 'Выберите на кого настроить переадресацию', reply_markup=markup)
+
+@bot.callback_query_handler(func=lambda call: call.data.split(':')[0] == 'phone')
+def phone_handler(call):
+    phone_number = call.data.split(':')[1]
+    os.environ['WDM_LOG_LEVEL'] = '0'
+    url = "https://msk.tele2.ru/"
+    options = webdriver.ChromeOptions()
+    options.add_experimental_option('excludeSwitches', ['enable-logging'])
+    driver = webdriver.Chrome(service=Service("chromedriver.exe"), options=options)
+    try:
+        driver.get(url=url)
+        time.sleep(1)
+        driver.delete_all_cookies()
+        for cookie in pickle.load(open(f"cookies_tele2", "rb")):
+            driver.add_cookie(cookie)
+        driver.get("https://msk.tele2.ru/lk/settings/callforwarding-managment")
+        cookie_check = driver.find_element_by_xpath('//*[@id="root"]/div/div[1]/div/div/div/div/div[1]/div/div/div/div/section/header/section[1]/div[3]/div/div/a/span')
+        if cookie_check.text == 'Войти':
+            #loginbutton = driver.find_element_by_xpath('//*[@id="root"]/div/div[1]/div/div/div/div/div[1]/div/div/div/div/section/header/section[1]/div[3]/div/div/a/span')
+            #loginbutton.click()
+            #time.sleep(2)
+            loginbox = driver.find_element_by_xpath('//*[@id="keycloakAuth.phone"]')
+            loginbox.send_keys(tele2_number)
+            time.sleep(1)
+            loginbutton = driver.find_element_by_xpath('//*[@id="keycloakLoginModal"]/div/div/div/div/div[2]/div/form/div[2]/button')
+            loginbutton.click()
+            time.sleep(1)
+            loginbutton = driver.find_element_by_xpath('//*[@id="keycloakLoginModal"]/div/div/div/div/div[2]/div/div/div[2]/button')
+            loginbutton.click()
+            time.sleep(1)
+            loginbox = driver.find_element_by_xpath('//*[@id="keycloakAuth.password"]')
+            loginbox.send_keys(tele2_password)
+            time.sleep(1)
+            loginbutton = driver.find_element_by_xpath('//*[@id="keycloakLoginModal"]/div/div/div/div/div[2]/div/form/div[2]/button[1]')
+            loginbutton.click()
+            time.sleep(2)
+            pickle.dump(driver.get_cookies(), open(f"cookies_tele2", "wb"))
+
+        driver.get("https://msk.tele2.ru/lk/settings/callforwarding-managment")
+        time.sleep(1)
+        phonebox = driver.find_element(By.XPATH, '//*[@id="042c52bb8ed236b5eb5b22ecea0e4753"]')
+        phonebox.clear()
+        time.sleep(1)
+        phonebox.send_keys(phone_number)
+        time.sleep(1)
+        acceptbutton = driver.find_element(By.XPATH, '//*[@id="root"]/div/div[1]/div/div/div/div/div[2]/div/div/div[2]/section/div/div[7]/button[1]')
+        acceptbutton.click()
+        time.sleep(2)
+
+        xpath_try1 = driver.find_elements(By.XPATH, "//*[text()='Настройки сохранены.']")
+        if not xpath_try1:
+            print("null")
+            errorbox = driver.find_element(By.XPATH, '//*[@id="root"]/div/div[1]/div/div/div/div/div[2]/div/div/div[2]/section/p[2]')
+            errorbox = errorbox.text
+            bot.send_message(call.from_user.id, f'{errorbox}')
+        else:
+            print("nonull")
+            bot.send_message(call.from_user.id, f'Переадресация выполнена\nПросьба проверить +7{tele2_number}')
+    except Exception as ex:
+        print(ex)
+    finally:
+        driver.close()
+        driver.quit()
 
 
 
